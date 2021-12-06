9b9ea59d641639cbb54ceef0596e1a66082db5ff
Karan Ahlawat <ahlawatkaran12@gmail.com>
2021-12-07 00:32:56 +0530

    [add] working script

--- .config/scripts/battery.lisp
+++ .config/scripts/battery.lisp
@@ -1,6 +1,8 @@
-#!/usr/bin/env sbcl
+#!/usr/bin/env -S sbcl --script
 
-(defun notify-idiom ()
+(require 'uiop)
+
+(defun notify-battery ()
   (let* ((parsed-val (with-open-file (in "/sys/class/power_supply/BAT0/capacity")
                        (read-line in nil nil)))
          (bat-level (parse-integer parsed-val)))
@@ -8,3 +10,5 @@
         (uiop:run-program "notify-send --urgency=normal 'Battery full' 'Battery has been charged to 90%. Please switch off.'"))
     (if (<= bat-level 10)
         (uiop:run-program "notify-send --urgency=normal 'Battery low!' 'Battery is below 10%. Please plug in.'"))))
+
+(notify-battery)
