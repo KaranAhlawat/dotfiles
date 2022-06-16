a62fb8fd12886f241041dc37101b0b4bd016adfe
Karan Ahlawat <ahlawatkaran12@gmail.com>
2022-06-17 00:19:48 +0530

    [add] back to tracking nvim

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 
 return {
   font = wezterm.font("PragmataPro Mono"),
-  font_size = 16,
+  font_size = 15,
   color_scheme = "Atom",
   exit_behavior = "Close"
 }
