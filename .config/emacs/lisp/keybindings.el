d1afc97cfc748151d53ff66248616123d7dde762
Karan Ahlawat <ahlawatkaran12@gmail.com>
2023-08-05 18:10:01 +0530

    added keybindings.el

---
+++ .config/emacs/lisp/keybindings.el
@@ -0,0 +1,25 @@
+;;; keybindings.el --- Convenient keybindings for Emacs -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Keybindings I had setup but forgot to add to yadm lmao
+;;; Code:
+
+(use-package god-mode
+  :straight t
+  :bind (:map god-local-mode-map
+              ("i" . god-local-mode)
+              ("." . repeat))
+  :config
+  (god-mode-all))
+
+(defun conf/delete-ws-backward-till-char ()
+  "Delete whitespace backwards till it encounters a character."
+  (interactive)
+  (save-excursion
+    (delete-region
+     (point)
+     (re-search-backward (rx (not space))))))
+
+(keymap-global-set "M-\\" #'conf/delete-ws-backward-till-char)
+
+(provide 'keybindings)
+;;; keybindings.el ends here
